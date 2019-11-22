181025c962e82dc433ffbe2654a65335bea95c95
Nate Powers <russellsproutz@gmail.com>
2019-11-22 09:40:27 -0800

    start API call tests

--- src/betterDoc-service.js
+++ src/betterDoc-service.js
@@ -1,7 +1,7 @@
 export class BetterDocService {
   async getArt(art) {
     try {
-      let response = await fetch(`https://api.harvardartmuseums.org/image?apikey=${process.env.API_KEY}&size=3&sort=random&baseimageurl`);
+      let response = await fetch('https://api.betterdoctor.com/2016-03-01?doctor_uid&api_key=0814daadd41f7d22d4721315d9ed7c8d');
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
