9481abf84a347f22748fc268200fff5a7f5936df
Nate Powers <russellsproutz@gmail.com>
2019-11-22 16:53:31 -0800

    begin building functions for input

--- src/betterDoc-service.js
+++ src/betterDoc-service.js
@@ -1,16 +1,24 @@
 export class BetterDocService {
-  async getDoc(doctor) {
+  async get(url,) {
     try {
-      let response = await fetch('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=0814daadd41f7d22d4721315d9ed7c8d', {
-      }
-  );
+      let response = await fetch(url)
+
   let jsonifiedResponse = await response.json();
   console.log(response);
   return jsonifiedResponse;
 } catch(error) {
-  console.error("The doctor is NOT in.....: " + error.message);
-
+  console.error("The doctor is NOT in.....: " + error.message)
 
     }
   }
 }
+
+
+  class SearchDoctor {
+constructor (docSearch) {
+this.docSearch = input().val
+
+}
+ }
+
+// 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=0814daadd41f7d22d4721315d9ed7c8d',
